dc8ab40675488bb6b7c2e37dbf32463f38a23809
Jonathan Yee <raptrex@gmail.com>
2023-02-09 16:54:51 -0800

    Initial Commit

---
+++ Pokemon/PokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonApp.swift
+//  Pokemon
+//
+//  Created by Jonathan Yee on 2/9/23.
+//
+
+import SwiftUI
+
+@main
+struct PokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
